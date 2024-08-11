02dadf78a7abef6b174c09f64cac293d5bd720b7
root <root@t1v-n-1e894c15-w-0.europe-west4-b.c.felafax-training.internal>
2024-08-11 22:43:17 +0000

    [gemma2 train, setup.py] training working, add one tokenizer false flag to setup

--- gemma2/trainer_engine/setup.py
+++ gemma2/trainer_engine/setup.py
@@ -4,11 +4,13 @@ import os
 def setup_environment():
     os.environ['HF_HUB_CACHE'] = '/mnt/persistent-disk/hf/'
     os.environ['HF_HOME'] = '/mnt/persistent-disk/hf/'
+    os.environ['TOKENIZERS_PARALLELISM'] = 'false'
     
     # Note: The following shell commands won't work directly in Python
     # We'll use os.system to execute them
     os.system('export HF_HUB_CACHE="/mnt/persistent-disk/hf/"')
     os.system('export HF_HOME="/mnt/persistent-disk/hf/"')
+    os.system('export TOKENIZERS_PARALLELISM=false')
     
 
 def setup_imports():
