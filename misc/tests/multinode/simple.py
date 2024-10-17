4ebc5eac65d4a178656b68ecba935c7f093beffb
Felarof <nithin.sonti@gmail.com>
2024-10-17 16:52:20 -0700

    testing shard map

--- misc/tests/multinode/simple.py
+++ misc/tests/multinode/simple.py
@@ -12,38 +12,48 @@ from jax.experimental.shard_map import shard_map
 devices = mesh_utils.create_device_mesh((4, 2))
 mesh = Mesh(devices, axis_names=('x', 'y'))
 
-
-# @partial(shard_map,
-#          mesh=mesh,
-#          in_specs=(P('x', 'y'), P('y', None)),
-#          out_specs=P('x', None))
-# def matmul_basic(a_block, b_block):
-#     c_partialsum = jnp.dot(a_block, b_block)
-#     c_block = jax.lax.psum(c_partialsum, 'y')
-#     return c_block
-
-def matmul_basic(a, b):
-    c = jnp.dot(a, b)
-    return c
-
-matmul_basic_jitted = jax.jit(
-    matmul_basic,
-    in_shardings=(P('x', 'y'), P('y', 'x')),
-    out_shardings=P(),
-)
+@partial(shard_map,
+         mesh=mesh,
+         in_specs=(P('x', 'y'), P('y', None)),
+         out_specs=P('x', None))
+def matmul_basic(a_block, b_block):
+    c_partialsum = jnp.dot(a_block, b_block)
+    c_block = jax.lax.psum(c_partialsum, 'y')
+    return c_block
 
 if jax.process_index() == 0:
     a = jnp.arange(8 * 16).reshape(8, 16)
     b = jnp.arange(16 * 8).reshape(16, 8)
-
     c = matmul_basic(a, b)
-    print("matmul_basic", c) 
-    
-    a_sharding = NamedSharding(mesh, P('x', 'y'))
-    b_sharding = NamedSharding(mesh, P('y', 'x'))
 
-    a = jax.device_put(a, a_sharding)
-    b = jax.device_put(b, b_sharding)
+    # Gather c from all devices to the host
+    c_gathered = jax.device_get(c)
+    print("Gathered result:")
+    print(c_gathered)
 
-    c = matmul_basic_jitted(a, b)
-    print("matmul_basic_jitted", c)
+
+# def matmul_basic(a, b):
+#     c = jnp.dot(a, b)
+#     return c
+
+# matmul_basic_jitted = jax.jit(
+#     matmul_basic,
+#     in_shardings=(P('x', 'y'), P('y', 'x')),
+#     out_shardings=P(),
+# )
+
+# if jax.process_index() == 0:
+#     a = jnp.arange(8 * 16).reshape(8, 16)
+#     b = jnp.arange(16 * 8).reshape(16, 8)
+
+#     c = matmul_basic(a, b)
+#     print("matmul_basic", c)
+
+#     a_sharding = NamedSharding(mesh, P('x', 'y'))
+#     b_sharding = NamedSharding(mesh, P('y', 'x'))
+
+#     a = jax.device_put(a, a_sharding)
+#     b = jax.device_put(b, b_sharding)
+
+#     c = matmul_basic_jitted(a, b)
+#     print("matmul_basic_jitted", c)
