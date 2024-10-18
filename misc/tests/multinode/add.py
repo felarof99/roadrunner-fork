6d39c096ee33b86a7a287d47c3ce5dd399381447
Felarof <nithin.sonti@gmail.com>
2024-10-17 17:08:25 -0700

    minor update

--- misc/tests/multinode/add.py
+++ misc/tests/multinode/add.py
@@ -1,5 +1,6 @@
 from functools import partial
 import jax
+
 jax.distributed.initialize()
 print(
     f"Process index: {jax.process_index()}, Device count: {jax.device_count()}"
@@ -12,6 +13,7 @@ from jax.experimental import mesh_utils
 devices = mesh_utils.create_device_mesh((4, 2))
 mesh = Mesh(devices, axis_names=('x', 'y'))
 
+
 def add_basic(a, b):
     c = a + b
     return c
@@ -19,7 +21,9 @@ def add_basic(a, b):
 
 add_basic_jitted = jax.jit(
     add_basic,
-    in_shardings=(P('x', 'y'), P('x', 'y')),
+    in_shardings=(NamedSharding(mesh,
+                                P('x', 'y')),
+                  NamedSharding(mesh, P('x', 'y'))),
 )
 
 # Create and shard data on all processes
